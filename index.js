678b7d908b04c09408e8842feccaa7ed2d6dc277
Vlabson Viana <vlabsonsviana@gmail.com>
2021-12-13 15:46:01 -0300

    atualização
    relembrando alguns comandos

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log("vlabson");
+console.log('vlabson');
+console.log(`vlabson`);
+
+$nome = "Vlabson";
+var data = new Date();
+
+console.log(`Ola!! Meu nome é ${$nome}. Estou aprendendo javaScript as ${data.getHours()}`);
