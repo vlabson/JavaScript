11e1e61036be5afd47925207b32a61ae1b8829ba
Vlabson Viana <vlabsonsviana@gmail.com>
2021-12-26 02:35:47 -0300

    atualização
    correção no nome da pasta

---
+++ aula15/index.js
@@ -0,0 +1,9 @@
+let num1 = 9.54578;
+//let num2 = Math.floor(num1); // arredonda para baixo
+//let num2 = Math.ceil(num1); // arredonda para cima
+//let num2 = Math.round(num1); // aredonda para o mais proximo ou seja se o numero estiver acima da metade vai pra cima abaixo da metade vai pra baixo
+
+//console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); //retorna o maior numero passado.
+//console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); //retorna o menor numero passado.
+const aleatorio = Math.random() * (10 - 5) + 5;//retorna um numero aleatorio entre 0 e 1
+console.log( Math.round(aleatorio)); 
