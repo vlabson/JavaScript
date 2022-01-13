e24d9fb5e5f8a0310bc837d6f71bb6bed0d8cd74
Vlabson Viana <vlabsonsviana@gmail.com>
2022-01-13 00:14:53 -0300

    update
    inclusao de comentarios e melhoramento na atividade e companhamento dos estudos

--- aula06/index.js
+++ aula06/index.js
@@ -1,14 +1,71 @@
-const nome = 'Luiz Otavio';
-const sobreNome = 'Miranda';
-const idade = 30;
-const peso = 84;
-const alturaEmM = 1.80;
+let nomeCliente = 'Joao'; // declaração normal 
+console.log(nomeCliente); // imprimindo no console
 
-let imc = peso/(alturaEmM * alturaEmM);
-let anoNascimento = 2019 - idade ;
 
-console.log(imc);
+/*
+ NÃO UTILIZE VAR UTILIZE SEMPRE LET!!!!!!!!!!!!!
+*/
 
-console.log(`${nome} ${sobreNome} tem ${idade} anos, pesa ${peso} kg `);
-console.log(`tem ${alturaEmM} de altura e seu IMC é de ${imc}`);
-console.log(`${nome} nasceu em ${anoNascimento}`);
+/*
+na declaração as variaveis SÃO CASE-SENSITIVE
+*/
+let nomeCliente = 'Luiz';  // esta variavel 
+let nomecliente = 'Otavio'; // é diferente desta variavel onde em uma temos o C maiusculo e na outra o c minusculo.
+
+
+/*
+forma correta de declaração de variavel com nomes compostos se utiliza o camelCase
+
+let nomeCliente;
+*/
+
+let nomeDoClieteCompleto = 'Vlabson da Silva Viana';
+
+/*
+ nomes das variaveis nao podem comte espaços ou traços!!  
+
+ let nome cliente; ERRADOOOOO
+*/
+
+/*
+não se pode começar o nome de uma variavel com um numero !!
+
+let 12Clientes;  ERRAAAAADOOOO
+*/
+
+// NÃO PODEMOS criar variaveis com palavras reservadas
+/* exemplos
+
+*  let let 
+*  let const
+*  let if
+*   
+* Variaceis precisam ter nomes significativos 
+* exemplo de como nao declarar 
+
+* let N = 'joao'; 
+* let A = 05/01/2022;
+* let X = 12346;
+* dessa forma NAO tem como se ter compreensão e entendimento da variavel.
+*
+*/
+
+/*let nome; // declarou uma variavel 
+nome = 'João'; // inicializando a variavel
+console.log(nome); // imprimindo no console
+nome = 'Otavio';
+console.log(nome);
+*/
+
+// let nome - uma variavel nao pode ser declarada Duas vezes !!!!
+
+/*
+let nome = 'Vlabson'; // String
+
+console.log(nome,'nasceu em 1984');
+console.log('Em 2000', nome, 'conheceu Maria');
+console.log(nome, 'casou-se com Maria em 2012');
+console.log('Maria teve 1 filho com ', nome, 'em 2015');
+console.log('O filho de', nome, 'se chama eduardo');
+
+*/
