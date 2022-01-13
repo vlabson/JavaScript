a5ec12163428c8c49b393b1d4200e4f1b78b7875
Vlabson Viana <vlabsonsviana@gmail.com>
2022-01-13 00:15:35 -0300

    update
    inclusao de uma condicional if

--- aula11/js/scritp.js
+++ aula11/js/scritp.js
@@ -1,6 +1,13 @@
 const num1 = parseInt(prompt('Digite Primeiro Numero!'));
 const num2 = parseInt(prompt('Digite Segundo Numero!'));
 
+
 const result = num1 + num2;
 
-alert(`O Resultado da Operação é: ${result} `);
+const confirmacao = confirm(`soma: ${result} este resultado esta correto?`);
+
+if(!confirmacao){
+    alert(`O Resultado da Operação: ${result} esta errado`);
+}else{
+       alert(`O Resultado da Operação: ${result} esta correto`);
+}
