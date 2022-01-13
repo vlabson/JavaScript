5f7397b7c748403c00cf8c6d7b3e1e6f1bb6bb39
Vlabson Viana <vlabsonsviana@gmail.com>
2022-01-13 00:11:43 -0300

    update

--- aula03/index.js
+++ aula03/index.js
@@ -1,6 +1,8 @@
-console.log("vlabson");
+console.log("vlabson"); // FUNÇÃO/METODO PARA IMPRIMIR NO CONSOLE 
 console.log('vlabson');
 console.log(`vlabson`);
+
+console.log(123456, 12.85, 'Vlabson viana');
 
 $nome = "Vlabson";
 var data = new Date();
