ea48e3bec850c7290b3204aa9debd15880adb421
Vlabson Viana <vlabsonsviana@gmail.com>
2022-01-27 21:17:20 -0300

    create

---
+++ aula21/index.js
@@ -0,0 +1,31 @@
+/*
+
+operadores de comparação
+
+> maior que
+>= maior que ou igual a
+< menor que 
+<= menor que ou igual 
+== igualdade  ( valor ) **** não se usa ****
+=== igualdade estrita ( valor e tipo )
+!= diferente que ( valor ) **** não se usa ****
+!== diferente estrito ( valor e tipo )
+
+let comp = 10 > 5; /* este operador faz a comparação se o dado da esquerda é maior que o da direita  
+console.log(comp);
+
+let comp = 10 >= 5;  este operador faz a comparação se o dado da esquerda é MAIOR ou IGUAL que o da direita  
+console.log(comp);
+ 
+let comp = 10 < 5;  este operador faz a comparação se o dado da esquerda é MENOR  que o da direita
+console.log(comp);
+
+let comp = 10 <= 5;  este operador faz a comparação se o dado da esquerda é MENOR ou IGUAL que o da direita
+console.log(comp);
+
+let comp = 10 == 5;  este operador faz a comparação se o dado da esquerda é IGUAL que o da direita
+console.log(comp);
+
+let comp = 10 == 5;  este operador faz a comparação se o dado da esquerda é IGUAL que o da direita
+console.log(comp);
+*/
